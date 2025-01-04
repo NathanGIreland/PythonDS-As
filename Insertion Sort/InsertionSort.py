9090be6a24b983cfb018c2bc95234a2abc415064
Nathan Ireland <nathangireland@gmail.com>
2025-01-04 13:51:49 -0600

    Comments to InsertionSort.py

--- Insertion Sort/InsertionSort.py
+++ Insertion Sort/InsertionSort.py
@@ -1,13 +1,14 @@
 def insertionSort(L, n):
-    for i in range(n):        
-        key = L[i]
-        j = i - 1
-        
+    for i in range(n):      
+        key = L[i] ## Key = Value of the current biggest element in the array
+        j = i - 1 ## j = The index of the value on the left of the biggest current element saved into a second pointer
+
+        ## Loops while the second pointer, j, is bigger than -1 and the value is L[j] is bigger than key (current biggest)
         while j >= 0 and L[j] > key:
-            L[j+ 1] = L[j]
-            j = j - 1
+            L[j+ 1] = L[j] ## If L[j] is bigger than the key it places it to the right of the Key
+            j = j - 1 ##Moves the j back into position of the current index in sorted portion
         
-        L[j + 1] = key
+        L[j + 1] = key ##Places the key to the right of the last element in the sorted array
         
     return L
     
